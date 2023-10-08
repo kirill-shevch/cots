8a2d1fd716ce5a9009c3e46f2684a3815a410749
Kirill Shevchenko <kirillwowlol@yandex.ru>
2023-10-08 20:02:21 +0200

    Add rigidbody movement

--- Assets/Scripts/Routing.cs
+++ Assets/Scripts/Routing.cs
@@ -7,35 +7,55 @@ public class Routing : MonoBehaviour
 {
     // Start is called before the first frame update
     GameObject target;
+    Rigidbody2D rb2D;
+
     Stack<(int x, int y)> path;
-    (int x, int y) position;
+    (int x, int y) nextPoint;
+    Vector2 nextStep;
+    Vector2 velocity;
 
     void Start()
     {
-        position = (1, 1);
         path = new Stack<(int x, int y)>();
+        rb2D = GetComponent<Rigidbody2D>();
+        GetTarget(1, 1);
+        velocity = GetVelocity();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (Vector2.Distance(nextStep, transform.position) < 0.1f)
         {
             if (path.Count == 0)
             {
-                GetTarget();
+                GetTarget(nextPoint.x, nextPoint.y);
             }
-            else
-            {
-                position = path.Pop();
-                Debug.Log($"NextStep: x-{position.x} ; y-{position.y}");
-                transform.position = new Vector2(position.x + 0.5f, position.y + 0.5f);
-                //transform.Translate(new Vector2(position.x + 0.5f, position.y + 0.5f));
-            }
+            rb2D.velocity = Vector2.zero;
+            velocity = GetVelocity();
         }
+
+        rb2D.MovePosition(rb2D.position + velocity * Time.fixedDeltaTime * 4);
     }
 
-    private void GetTarget() 
+    private Vector2 GetVelocity()
+    {
+        float diagonalModifyer = 1;
+        nextPoint = path.Pop();
+        nextStep = new Vector2(nextPoint.x + 0.5f, nextPoint.y + 0.5f);
+        if (Mathf.Abs(nextStep.x - transform.position.x) > 0.1 && Mathf.Abs(nextStep.y - transform.position.y) > 0.1)
+        {
+            diagonalModifyer = 0.7071f;
+        }
+        return new Vector2((nextStep.x - transform.position.x) * diagonalModifyer, (nextStep.y - transform.position.y) * diagonalModifyer);
+    }
+
+    private void GetTarget(int currentX, int currentY) 
     {
         if (target != null)
         {
@@ -53,7 +73,7 @@ public class Routing : MonoBehaviour
             target = World.Build(1, 1, xCord, yCord);
             Debug.Log($"Target: x-{xCord} ; y-{yCord}");
             target.GetComponent<SpriteRenderer>().color = Color.yellow;
-            path = World.FindShortestPath(position, (xCord, yCord));
+            path = World.FindShortestPath((currentX, currentY), (xCord, yCord));
         }
     }
 }
